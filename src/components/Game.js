c788e4db75aedd3523945b517a13e99c15fd11e0
arifinkhoirul <irul30582@gmail.com>
2023-11-04 22:38:53 +0700

    week 12

---
+++ src/components/Game.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Board from "./Board";
+
+function Game() {
+	return (
+		<div>
+			<div>
+				<Board />
+			</div>
+		</div>
+	);
+}
+
+export default Game;
---
+++ src/components/Square.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Square({ value, onClick }) {
+	const colorClass = value === "X" ? "text-green-500" : "text-red-500";
+
+	return (
+		<button className={`square focus:outline-none ${colorClass} shadow-md border-player`} onClick={onClick}>
+			<div className="flex items-center justify-center h-full">
+				{value}
+			</div>
+		</button>
+	);
+}
+
+export default Square;
